b66b3aacf8db68d3ded54377f6668dde967a137f
Ismael Ignacio Morinigo <thecoderbutcher@gmail.com>
2025-06-08 02:12:36 -0300

    add list song

--- src/renderer/src/SongList/SongList.tsx
+++ src/renderer/src/SongList/SongList.tsx
@@ -1,13 +1,13 @@
-import ControlSong from './components/ControlSong'
+/* import ControlSong from './components/ControlSong' */
 import ListSongs from './components/ListSongs'
 import SearchSongs from './components/SearchSongs'
 
 function SongList(): React.JSX.Element {
   return (
     <div className="col-span-7 flex flex-col p-1 lg:p-2">
-      <SearchSongs />{/*
+      <SearchSongs />
       <ListSongs />
-      <ControlSong /> */}
+      {/*<ControlSong /> */}
     </div>
   )
 }
