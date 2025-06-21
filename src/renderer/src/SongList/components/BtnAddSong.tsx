e27630336066f0fbf203c6b80977c41ecbb65ea9
Ismael Ignacio Morinigo <thecoderbutcher@gmail.com>
2025-06-21 05:14:04 -0300

    delete modal add song and simplify add song using portal so

--- src/renderer/src/SongList/components/BtnAddSong.tsx
+++ src/renderer/src/SongList/components/BtnAddSong.tsx
@@ -1,19 +1,38 @@
+import { Song } from '@prisma/client'
+import { usePlaylists } from '@renderer/context/PlaylistContext/PlaylistHook'
+import { useSongs } from '@renderer/context/SongContext/SongHook'
 import { PiMusicNotesPlusFill } from 'react-icons/pi'
-import { AddSongModal } from './ModalAddSong'
-import { useState } from 'react'
 
 function BtnAddSong(): React.JSX.Element {
-  const [showModal, setShowModal] = useState(false)
-  const addsong = (): void => setShowModal(true)
+  const { playlistState } = usePlaylists()
+  const { songDispatch } = useSongs()
+
+  const addsong = async (): Promise<void> => {
+    const filePath = await window.electronAPI.openFile()
+
+    if (filePath) {
+      if (!playlistState.playlistSelected) return
+
+      const meta = await window.electronAPI.readMetadata(filePath)
+      const song: Song = await window.electron.ipcRenderer.invoke(
+        'song:add',
+        playlistState.playlistSelected?.id,
+        meta.title || 'Titulo desconocido',
+        meta.artist || 'Artista desconocido',
+        meta.album || 'Album desconocido',
+        meta.duration || 0,
+        meta.cover || 'none',
+        filePath
+      )
+      songDispatch({ type: 'ADD_SONGS', payload: song })
+    }
+  }
   return (
-    <>
-      <div className="flex items-center justify-center border border-accent text-accent p-1 text-base hover:scale-125 hover:text-primary hover:bg-accent rounded-md transition-all duration-200">
-        <button onClick={addsong}>
-          <PiMusicNotesPlusFill />
-        </button>
-      </div>
-      {showModal && <AddSongModal onClose={() => setShowModal(false)} />}
-    </>
+    <div className="flex items-center justify-center border border-accent text-accent p-1 text-base hover:scale-125 hover:text-primary hover:bg-accent rounded-md transition-all duration-200">
+      <button onClick={addsong}>
+        <PiMusicNotesPlusFill />
+      </button>
+    </div>
   )
 }
 
